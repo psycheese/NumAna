fab66bffb95969462c5810fd2b7ce6dcd80fd7db
chaiyuntian <tianyunchai@126.com>
2015-04-08 21:24:19 +0800

    migrate to team repo
    Hope this could be a team learning process.

---
+++ static/js/numana.js
@@ -0,0 +1,114 @@
+/**
+ * Created by Yuntian Chai on 15-3-30.
+ */
+
+/*
+* custom log function
+* @param m - log content
+* @param t - log either to console or to html document
+* */
+var log = function(m,t){
+    if(!t){console.log(m);}
+    else{
+        document.write(m+'<br>');
+    }
+};
+
+
+
+/* use dichotomie to solve the equation of one unknown variable x.
+ * @param f -f(x)=0 equation expression on the left side
+ * @param l -start_guess for x in range [a, b]
+ * @param r -start_guess for x in range [a, b]
+ * @param t -tolerance for precision
+ * @param itl -iterate times
+ * @return estimation of x
+ */
+//
+// exp: f(x) = 0
+var dichotomie = function(f,l,r,t,it){
+    it = it||20;
+    if(f(l)*f(r)>=0.0){
+        log("please make sure the start value setting is right:f(l)*f(r)<=0");
+        return;
+    }
+    var a = l,b = r,i=0;
+    while((b-a)/2.0>t)
+    {
+        var c = (a+b)/2.0;
+        if(f(c)==0.0){return c;}
+        if(f(c)*f(a)<0.0){b=c;}
+        else{a = c;}
+        i++;if(i>it){break;}
+    }
+    return (a+b)/2.0;
+};
+
+/* Fixed Point Iteration
+ * @param f - f(x) = x equation expression on the left side
+ * @param x0 - start point of x
+ * @param n - number of iterations
+ * @return estimation of x
+ */
+var FPI = function(f, x0, n)
+{
+    var a = x0,i;
+    for(i=0;i<n;i++){a = f(a);}
+    return a;
+};
+
+
+/* Newton Iteration
+ * @param f - f(x) = 0 equation expression on the left side
+ * @param f1 - f'(x): first-order derivative of f(x)
+ * @param x0 - start point of x
+ * @param n - number of iterations
+ * @return estimation of x
+ */
+var newton = function(f, f1, x0, n)
+{
+    var a = x0,i;
+    for(i=0;i<n;i++){a = a-f(a)/f1(a);}
+    return a;
+};
+
+
+/* Secant Method
+ * @param f - f(x) = 0 equation expression on the left side
+ * @param x0 - 1st start point
+ * @param x1 - 2st start point
+ * @param n - number of iterations
+ * @return estimation of x
+ */
+var Secant = function(f,x0,x1,n){var i,a=x0,b=x1,c=x0;for(i=0;i<n;i++){var m=f(a);var n=f(b);c=(b*m-a*n)/(m-n);if(c==0){return c;}if(m*f(c)<0){b = c;}else{a = c;}}return c;};
+
+
+eqsolve = {
+    dic:dichotomie,
+    fpi:FPI,
+    nwt:newton,
+    sec:Secant
+};
+
+
+
+var Exec = function(f){
+    var d = new Date();
+    var st,ed,delta;
+    var r;
+
+    st= (new Date()).getMilliseconds();
+    r = f();
+    ed= (new Date()).getMilliseconds();
+    if(r){}else{r="NULL";}
+
+    delta = ed-st;
+
+    log("Result:"+r+","+"Time:"+delta,true);
+};
+
+
+
+/*TODO*/
+// Add Equation Group And Matrix
+/* */
---
+++ static/js/test.js
@@ -0,0 +1,67 @@
+/**
+ * Created by Yuntian Chai on 15-3-30.
+ */
+
+/*TODO: Add unit test for each function*/
+
+var test = {};
+
+test.dichotomie = function()
+{
+    var fx = function(x){
+        return x*x*x+x-1;
+    };
+
+    log(eqsolve.dic(fx,0,1,0.00005),true);
+    log(eqsolve.dic(fx,0,1,0.00005,10),true);
+    log(eqsolve.dic(fx,0,1,1E-10,100),true);
+};
+
+//test.dichotomie();
+
+test.FPI = function()
+{
+    var fx1 = function(x){
+        return 1.0-x*x*x;
+    }; // Failed to converge，either at x0 = 0 or x0 = 1.
+
+
+    var fx2 = function(x){
+        return Math.pow(1.0-x,1/3.0);
+    }; // 0.5 is a good start point but 0 is not.
+
+    log(eqsolve.fpi(fx2,0.5,20),true);
+
+};
+
+test.Newton = function()
+{
+    var fx = function(x){
+        return x*x*x + x -1.0;
+    };
+    var f1x = function(x){
+        return 3.0*x*x + 1.0;
+    };
+
+    Exec(function(){
+        return eqsolve.nwt(fx,f1x,0.7,20);
+    });
+
+};
+
+test.Secant = function()
+{
+    var fx = function(x){
+        return x*x*x -2*x*x + 1.5*x;
+    };
+
+    Exec(function(){
+            for(var i = 0;i<50000;i++){eqsolve.sec(fx,-1.0,1.0,i);}
+        }
+    )
+
+};
+
+
+test.Secant();
+
