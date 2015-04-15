c8777fd2ec224fd4e2a17c10f99948bc485621f7
sperlingxx <lovedreamf@gmail.com>
2015-04-15 21:06:45 +0800

    Update matrix.js

--- static/js/matrix.js
+++ static/js/matrix.js
@@ -3,31 +3,36 @@
 var matrix = function(args){
 
     this.d = args; // dimensions
-    this.ia = []; // index arrays
+    this.ia = new Array(args.length); // index arrays
 
     var i= 0,j=0;
-    var L=1;
-    for(i=0;i<args.length;i++)
-    {
+    var L= 1,M=args.length;
+    var X=new Array(args.length);
+    for(i=0;i<M;i++) {
         L *= args[i]; // length calculation
+        X[i]=0;
+        this.ia[i]=[];
     }
 
     // initialize the index array
-    var k=1;
-    for(i=0;i<args.length;i++)
+    var pt= 0,n=0;
+    for(n=0;n<L;n++)
     {
-
-        this.ia.push([]);
-        k *= args[i];
-        for(j=0;j<L;j++)
-        {
-           this.ia[i].push(parseInt(j/k));
+        for(i=0;i<M;i++)
+            this.ia[i].push(X[i]);  //将第n+1种排列赋值给ia
+        while(X[pt]>=this.d[pt]-1){ //进位
+            X[pt]=0;
+            pt++;
         }
+        X[pt]++;  //X的值增加1
+        pt=0; //归位
     }
-
 
 };
 
 matrix.prototype.item = function(d){
 
 };
+
+//var m=new matrix([5,5,6,11]);
+//console.log("matrix:",m);
